d86d3bba9aaae6d306939042a75121fa73ee16e4
Jackwasson <objectivezt@gmail.com>
2017-04-10 19:29:58 +0800

    'node'

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by admin on 2017/4/10.
+ */
+console.log('hellojs');
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
---
+++ config/database.js
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 2017/4/10.
+ */
+module.exports = {
+    'connection': {
+        'host' : '120.24.244.81',
+        'user' : 'root',
+        'password' : '666996'
+    },
+    'database' : 'bbs_develop',
+    'users_table' : 'users'
+}
